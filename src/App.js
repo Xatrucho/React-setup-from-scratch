eb90670467129bcad7acc6ee7bcf7681e606e060
Xatrucho <marlonenriqueponcevillatoro@gmail.com>
2022-01-18 11:31:46 -0600

    updated files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "App.css";
+import coding from './cp-coding-bg.jpeg';
+
+const App = () => (
+    <div>
+        <h1 className="wow">
+            Hello Champion!
+        </h1>
+        <img src={coding} alt="" />
+    </div>
+);
+
+export default App;
